b4125494684d425b194a5cc18cf19bf3fb991a58
vishalT1999 <vishal24tanwar@gmail.com>
2022-04-05 13:39:26 +0530

    localstorage remove done

--- src/pages/CartItem.js
+++ src/pages/CartItem.js
@@ -5,7 +5,6 @@ import { CART_ITEMS } from "../constant";
 const CartItem = () =>{
     
     const [itemCart,setItemCart] = useState([]);
-    //const [newItemCartList,setNewItemCartList] = useState(itemCart);
 
      useEffect(()=>{
         const item = localStorage.getItem(CART_ITEMS);
@@ -15,18 +14,20 @@ const CartItem = () =>{
      const hRemove = (item) =>{
         console.log(item);
         const index = itemCart.findIndex((it)=> it === item);
-        //const index = item;
         console.log(index);
         const NewItenList = [...itemCart.slice(0,index) , ...itemCart.slice(index+1)];
         setItemCart(NewItenList);
-         console.log(itemCart);
-        //setNewItemCartList(NewItenList);
+        //const NewRMData = JSON.parse(localStorage.getItem("cart_items")); 
+        //console.log(NewRMData); 
+        //NewRMData.splice(index,1); 
+        localStorage.setItem(CART_ITEMS ,JSON.stringify(NewItenList)); //NewItenList = NewRMData
+         //console.log(itemCart); 
+        //setNewItemCartList(NewItenList); 
         }
      
      return(
          <div>
              <h1>Hello cart page</h1>
-             {/* {itemCart.map(item=> <p>key:{item.key} value:{item}</p>)} */}
              {itemCart !== null ? itemCart.map(item => <p> Added items are : {item} <Button onClick={() => {hRemove(item)}} text="X"/> </p>) : []}
          </div>
      )
