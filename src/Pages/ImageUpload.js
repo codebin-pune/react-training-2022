b36a1607a2e48422bcbe96ca48a8f5d6a68f55cc
Gopalpatidar0012 <yogapuran0012@gmail.com>
2022-04-11 14:47:14 +0530

    image upload and cropper

--- src/Pages/ImageUpload.js
+++ src/Pages/ImageUpload.js
@@ -1,90 +1,64 @@
+import React, { useState } from "react";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
 
-import ReactCrop from "react-image-crop";
-import "react-image-crop/dist/ReactCrop.css"
-
-import { useState } from "react";
-
-const ImageUpload = () => {
- 
-
-  const [src,setSrc]=useState(null);
-  const [image,setImage]=useState()
-  const [crop,setCrop]=useState({aspect:16/9});
-  const [result,setResult]=useState(null);
-
-console.log(image)
-  const onChangePicture = (e) => {
+export const ImageUpload = () => {
+  const [image, setImage] = useState();
+  const [cropData, setCropData] = useState("#");
+  const [cropper, setCropper] = useState();
+  const onChange = (e) => {
     const url = URL.createObjectURL(e.target.files[0]);
-    setSrc(url);
+    setImage(url);
   };
-  //   const reader = new FileReader();
-  //   reader.addEventListener("load", () => {
-  //     setImgData(reader.result);
-  //     console.log(reader.result)
-  //   });
-  //   reader.readAsDataURL(e.target.files[0]);
-  // }
-  function getCroppedImage() {
-    // creating the cropped image from the source image
-    const canvas = document.createElement('canvas');
-    const scaleX = image.naturalWidth / image.width;
-    const scaleY = image.naturalHeight / image.height;
-    canvas.width = crop.width;
-    canvas.height = crop.height;
-    const ctx = canvas.getContext('2d');
 
-    ctx.drawImage(
-        image,
-        crop.x * scaleX,
-        crop.y * scaleY,
-        crop.width * scaleX,
-        crop.height * scaleY,
-        0,
-        0,
-        crop.width,
-        crop.height
-    );
-
-    // const base64Image= canvas.toDataURL("image/jpeg");
-    // setResult(base64Image);
-    // console.log(result)
-
-    canvas.toBlob(blob=>{
-        setResult(blob)
-    })
+  const getCropData = () => {
     
-  }
-  console.log(src)
+      setCropData(cropper.getCroppedCanvas().toDataURL());
+    
+  };
+
   return (
     <div>
-       <div className="register_profile_image" style={{ marginTop: 100 }}>
-        <input type="file" id="profilePic" accept="image/*"  onChange={onChangePicture} />
-       </div>
-       {/* <div className="previewProfilePic">
-        <img
-          className="playerProfilePic_home_tile"
-          src={src}
-          style={{ width: 400 }}
+      <div style={{ width: "100%", marginTop: 30, marginLeft: 30 }}>
+        <input type="file" onChange={onChange} />
+        <br />
+        <br />
+        <Cropper
+          style={{ width: "600px", height: "600px" }}
+          initialAspectRatio={1}
+          preview=".img-preview"
+          src={image}
+          viewMode={1}
+          minCropBoxHeight={10}
+          minCropBoxWidth={10}
+          background={false}
+          responsive={true}
+          autoCropArea={1}
+          checkOrientation={false}
+          onInitialized={(instance) => {
+            setCropper(instance);
+          }}
+          // guides={true}
         />
-      </div> */}
-     
-      
-     
-     {src &&  <div>
-        
-              <ReactCrop onImageLoaded={setImage(src)} crop={crop}  onChange={setCrop}>
-              <br/>
-              <img className="playerProfilePic_home_tile" src={src} style={{ width: 400 }} />
-              
-            </ReactCrop>
-        <button onClick={getCroppedImage}>crop Image</button>
-      </div> }
-
-      {
-          result && <div>
-              <img src={result} alt="cropped image"/>
+        <br />
+      </div>
+      <div>
+        <div className="box" style={{ marginLeft: 20 }}>
+          <div>
+            <button onClick={getCropData} style={{ marginLeft: 30 }}>
+              Crop Image
+            </button>
+            <br />
+            <br />
           </div>
-      }
+
+          <img
+            style={{ width: "300px", height: "300px" }}
+            src={cropData}
+            alt="cropped"
+          />
+        </div>
+      </div>
     </div>
   );
 };
